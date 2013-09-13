37a7572b8410fa039bd315f70f4515f4ae15de66
Ben Cardy <ben.cardy@ocado.com>
2013-09-13 13:49:34 +0100

    Upgraded to 1.10.3

--- jquery_ui/static/css/jquery-ui/foo.sh
+++ jquery_ui/static/css/jquery-ui/foo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm ._*
-rm jquery.ui.theme.css
-DIR=${PWD##*/}
-curl -o jquery-ui.css "http://code.jquery.com/ui/1.9.2/themes/$DIR/jquery-ui.css"
+for DIR in $(ls -l | grep ^d | awk '{ print $NF }')
+do
+    cd $DIR
+    curl -o jquery-ui.css "http://code.jquery.com/ui/1.10.3/themes/$DIR/jquery-ui.css"
+    cd ..
+done
